0880ba69ea47cd84052e45852d1badfaa193aeb6
Brandon Jimenez <jimenezchronic@yahoo.com>
2015-03-05 13:26:56 -0800

    got the "Add" button to work

--- app/src/main/java/android/bs/todo/Add_Activity.java
+++ app/src/main/java/android/bs/todo/Add_Activity.java
@@ -1,35 +1,19 @@
 package android.bs.todo;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class Todays_Activity extends Activity {
+public class Add_Activity extends Activity {
 
-    public Button backButton;
-    public  Button buttonA;
-
+    public Button buttonA;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todaylist);
-        backButton = (Button) findViewById(R.id.MenuB);
-
-        backButton.setOnClickListener( new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-
-                Intent intent = new Intent(getApplicationContext(), TodoActivity.class);
-
-                startActivity(intent);
-
-            }
-        } );
-
         buttonA = (Button) findViewById(R.id.AddB);
 
         buttonA.setOnClickListener( new View.OnClickListener(){
